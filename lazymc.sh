57e776c9f155e07e0e7e3ad5c90428d25d1c790e
raohag <rh@wiped.pw>
2022-11-06 14:38:58 +0100

    Initial commit

---
+++ lazymc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo 'Starting lazymc'
+echo 'Checking whether configuration exists... '
+
+if test -f "/lazymc/lazymc.toml"; then
+    echo "Config found, handing command over to lazymc!"
+else
+    echo "It doesn't! Generating..."
+    lazymc config generate
+    echo "If it doesn't work, please look into the documentation of lazymc to see how to generate or adapt the config."
+fi
+
+# Update start command to run docker instead
+sed -i '/command = /s/".*"/"docker start '$MC_CONTAINER_NAME'"/' /lazymc/lazymc.toml
+cat /lazymc/lazymc.toml | grep command
+
+lazymc
